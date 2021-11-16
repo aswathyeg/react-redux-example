7078e6d42d793729f7f502234c68c4e7426cffb4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-17 01:01:11 +0530

    added reducer from user

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
+import useReducer from './features/user'
 
 const store=configureStore({
-  reducer:{},
-})
+  reducer:{
+    user:useReducer,
+  },
+});
 ReactDOM.render(
   <Provider store={store}>
   <React.StrictMode>
