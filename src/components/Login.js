5b919314a017c7c5f3b431d3d67065f6bedcc08d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-18 11:41:53 +0530

    added dispatch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,13 @@
-
+import React  from "react";
 import { useDispatch} from "react-redux" //to modify values of state 
-import { login } from "./features/user"
+import { login } from "./features/user";
+
 export default function Login(){
+    const dispatch = useDispatch();
     return(
         <div>
-            <button>Login</button>
+            <button onClick={()=>{
+                dispatch(login({name:"jhon",age:30,email:"a@b.com"}))}}>Login</button>
         </div>
     )
 }
