30663c84eb4945ff55a45d6f74e48055155db896
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-18 16:40:48 +0530

    added logout to dispatch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,22 @@
-import React  from "react";
-import { useDispatch} from "react-redux" //to modify values of state 
+import React from "react";
+import { useDispatch } from "react-redux" //to modify values of state 
 import { login } from "./features/user";
+import { logout } from "./features/user";
 
-export default function Login(){
+export default function Login() {
     const dispatch = useDispatch();
-    return(
+    return (
         <div>
-            <button onClick={()=>{
-                dispatch(login({name:"jhon",age:30,email:"a@b.com"}))}}>Login</button>
+
+            <button onClick={() => {
+                dispatch(login({ name: "jhon", age: 30, email: "a@b.com" }))
+            }}>Login</button>
+
+
+
+            <button onClick={() => {
+                dispatch(logout())
+            }}>Logout</button>
         </div>
     )
 }
