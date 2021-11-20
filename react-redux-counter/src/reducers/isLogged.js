e7e338edb07eff8c6248a37187a71686a51c494f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-21 01:14:09 +0530

    return state

--- react-redux-counter/src/reducers/isLogged.js
+++ react-redux-counter/src/reducers/isLogged.js
@@ -1,7 +1,9 @@
-const loggedReducer=(state=false,action)=>{
+ const loggedReducer=(state=false,action)=>{
     switch (action.type){
     case 'isSignIn': 
-    return(!state)
+    return !state;
+    default:
+        return state;
     }
-}
+};
 export default loggedReducer;
