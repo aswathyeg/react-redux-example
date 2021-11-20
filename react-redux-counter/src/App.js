0817c20560b5772c6b25dd6e7e317f7b50c5d81e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-21 01:12:31 +0530

    added buttons and dispatch

--- react-redux-counter/src/App.js
+++ react-redux-counter/src/App.js
@@ -1,9 +1,17 @@
-
+import {useSelector,useDispatch} from 'react-redux';
+import {increment,decrement} from './actions';
 
 function App() {
+
+  const counter=useSelector(state=>state.counter);
+  const isLogged=useSelector(state=>state.isLogged);
+  const dispatch=useDispatch();
   return (
     <div className="App">
-     
+<h1>counter:{counter}</h1>
+     {isLogged?<h1>LoggedIn</h1>:""}
+     <button onClick={()=>dispatch(increment())}>+</button>
+     <button onClick={()=>dispatch(decrement())}>-</button>
     </div>
   );
 }
